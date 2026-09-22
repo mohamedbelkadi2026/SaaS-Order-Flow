d8f40dced054a6a4112f0c9219bf81bf53595e7d
mohamedbelkadi2026 <virtuelzon@gmail.com>
2026-09-22 15:56:19 +0100

    Use direct product selection in new order picker

--- client/src/pages/new-order-add.tsx
+++ client/src/pages/new-order-add.tsx
@@ -451,7 +451,6 @@ export default function NewOrderAdd() {
                     value={item.rawProductName}
                     onChange={p => handleProductSelect(item.id, p)}
                     placeholder="Rechercher dans le stock..."
-                    showVariants={false}
                   />
                 </div>
                 <Input className="text-xs h-9" placeholder="Référence" value={item.sku} onChange={e => updateItem(item.id, "sku", e.target.value)} />
